d65456cd2e96942716ceff411ca8322bce898731
Frank Chau <frank.chau@wellintech.com.hk>
2018-04-23 18:29:17 +0800

    cover

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,15 @@
 //config constants
 export const WORDPRESS_URL = 'http://mobilecomic.com/';
 export const WORDPRESS_REST_API_URL = WORDPRESS_URL + 'wp-json/wp/v2/';
+
+/*
+import {AppConfig} from "safaassafsaf";
+
+export const AppConfig = {
+    WORDPRESS_URL: "fdsfssf",
+    WORDPRESS_REST_API_URL: "fefewef"
+};
+
+
+AppConfig.WORDPRESS_URL
+*/
